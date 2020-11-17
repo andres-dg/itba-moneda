3c68f8081c19e64a933f738365fba6b1b0412b85
andres-dg <adigiovanni1997@gmail.com>
2020-11-17 15:55:23 -0300

    final

--- snx_buyer.js
+++ snx_buyer.js
@@ -10,18 +10,20 @@ async function main() {
     const mnemonic = "firm truly cereal release system love charge million sound board save guess";
     const provider = new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/c6ea1fc224384fcbbd67c39852f9536b`)
 
+
+    const rate = 0.07232982/72329821279743179
     var web3 = new Web3(provider);
     web3.eth.defaultAccount = '0xc64F72dE9dca0600DbcC734491Cb318242D303E8';
 
     const SynthBuyer = new web3.eth.Contract(abi, '0x619DEeBA1a79907e66f0193f28cDa10683f6974a');
 
-    async function Buy() {
+    async function Buy(amount) {
         console.log("Checking balance before buying...");
         await Check();
 
-        snx = await SynthBuyer.methods.buySNX(1e14).send({from: web3.eth.defaultAccount, value: 1e15})
+        snx = await SynthBuyer.methods.buySNX(amount).send({from: web3.eth.defaultAccount, value: 1e15})
         .on('transactionHash', function(h){
-            console.log("got tx hash: " + h);
+            console.log("got tx hash: https://ropsten.etherscan.io/tx/" + h);
         })
         .on("receipt", function(){
             console.log("got receipt...");
@@ -36,10 +38,10 @@ async function main() {
 
     async function Check() {
         balance = await SynthBuyer.methods.getBalance().call();
-        console.log("balance: " + balance);
+        console.log("balance: " + balance*rate + " SNX");
     }
 
-    await Buy();
+    await Buy(1e14);
 }
 
 main();
